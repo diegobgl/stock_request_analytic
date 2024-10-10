f38f28f4535273b07049c176f7ae23da9fb9d60e
Diego Gajardo <deskodoo@a2group.cl>
2024-10-10 11:50:41 -0300

    sudo elevation permission

--- models/stock_request.py
+++ models/stock_request.py
@@ -11,23 +11,23 @@ class StockRequestOrder(models.Model):
     @api.model
     def create(self, vals):
         # Heredar las cuentas analíticas en las líneas de stock move y stock picking
-        res = super(StockRequestOrder, self).create(vals)
+        res = super(StockRequestOrder, self).sudo().create(vals)
         if res.analytic_account_ids:
-            res._assign_analytic_accounts()
+            res.sudo()._assign_analytic_accounts()
         return res
 
     def write(self, vals):
         # Al escribir en el registro, asegúrate de que las cuentas analíticas se actualicen en las líneas
-        res = super(StockRequestOrder, self).write(vals)
+        res = super(StockRequestOrder, self).sudo().write(vals)
         if 'analytic_account_ids' in vals:
-            self._assign_analytic_accounts()
+            self.sudo()._assign_analytic_accounts()
         return res
 
     def _assign_analytic_accounts(self):
         # Asignar cuentas analíticas a las líneas relacionadas
-        for order in self:
+        for order in self.sudo():
             if order.analytic_account_ids:
                 # Asignar a los movimientos de stock
-                order.move_ids.write({'analytic_account_ids': [(6, 0, order.analytic_account_ids.ids)]})
+                order.move_ids.sudo().write({'analytic_account_ids': [(6, 0, order.analytic_account_ids.ids)]})
                 # Asignar a los pickings
-                order.picking_ids.write({'analytic_account_ids': [(6, 0, order.analytic_account_ids.ids)]})
+                order.picking_ids.sudo().write({'analytic_account_ids': [(6, 0, order.analytic_account_ids.ids)]})
