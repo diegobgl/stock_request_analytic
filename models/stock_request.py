7f03b0dd48b264c0694ab478a60db6c438966daa
Diego Gajardo <deskodoo@a2group.cl>
2024-10-10 16:00:19 -0300

    33

--- models/stock_request.py
+++ models/stock_request.py
@@ -21,10 +21,22 @@ class StockRequestOrder(models.Model):
     
     def _assign_analytic_accounts(self):
         for order in self:
+            # Verifica si el picking ya fue creado
+            if not order.stock_picking_id:
+                # Crear el picking primero si es necesario
+                picking_vals = {
+                    'origin': order.name,  # Ajusta según sea necesario
+                    'move_type': 'direct',  # Directo o según la lógica que uses
+                    # Otros campos necesarios para crear el picking
+                }
+                picking = self.env['stock.picking'].create(picking_vals)
+                order.stock_picking_id = picking.id
+
+            # Asignar cuentas analíticas después de la creación del picking
             if order.analytic_account_ids:
                 order.stock_picking_id.analytic_account_ids = [(6, 0, order.analytic_account_ids.ids)]
             else:
-                order.stock_picking_id.analytic_account_ids = [(5, 0, 0)]  
+                order.stock_picking_id.analytic_account_ids = [(5, 0, 0)]  # Limpia si no hay cuentas analíticas
     # def _assign_analytic_accounts(self):
     #     # Asignar cuentas analíticas a las líneas relacionadas
     #     for order in self.sudo():
